7c1e95ddaed5e2dba7d4d276cfebde28b401da7d
EC2 Default User <ec2-user@ip-10-79-3-140.corp.novocorp.net>
2026-04-20 14:13:47 +0000

    joiner

---
+++ src/knime2py/nodes/joiner.py
@@ -0,0 +1,14 @@
+"""
+Joiner module for joining two tables
+
+Inputs:
+- 2 DataFrames
+
+Outputs:
+- Publishes DataFrame to the context output ports
+
+Configuration
+----------------------
+
+
+"""
